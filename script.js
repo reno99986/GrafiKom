1fa510bb81671d966b3bb55bf5bcb8be6b493fd6
Reno Fahrezi Purnomo <jamrock99986@gmail.com>
2024-02-06 20:49:32 +0800

    Memasukkan file kedalam Github

---
+++ script.js
@@ -0,0 +1,52 @@
+/**
+ * Description
+ * @type {HTMLCanvasElement} ".kertas"
+ *
+ */
+
+const kertas = document.querySelector(".kertas");
+const ctx = kertas.getContext("2d");
+
+const valueSlider = document.querySelector(".x1");
+const valueSlider1 = document.querySelector(".x2");
+valueSlider.setAttribute("max", `${window.innerHeight}`);
+valueSlider1.setAttribute("max", `${window.innerHeight}`);
+kertas.width = window.innerWidth;
+kertas.height = window.innerHeight;
+function drawDDA(x1, x2, y1, y2) {
+  const dx = x2 - x1;
+  const dy = y2 - y1;
+  if (Math.abs(dx) > Math.abs(dy)) {
+    steps = Math.abs(dx);
+  } else {
+    steps = Math.abs(dy);
+  }
+
+  // if ((steps = 0)) {
+  //   let incX = 0;
+  //   let incY = 0;
+  // } else {
+  let incX = dx / steps;
+  let incY = dy / steps;
+  // }
+  let x = x1;
+  let y = y1;
+  for (let i = 0; i < steps; i++) {
+    ctx.fillStyle = "red";
+    ctx.fillRect(Math.floor(x), Math.floor(y), 5, 5);
+
+    x += incX;
+    y += incY;
+  }
+}
+
+function slider() {
+  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  x1 = Number(document.querySelector(".x1").value);
+  x2 = Number(document.querySelector(".x2").value);
+  x1 = x1 * 5;
+  x2 = x2 * 5;
+  ctx.reset;
+  drawDDA(x1, x2, 1, 600);
+}
+drawDDA(300, 300, 1, 600);
